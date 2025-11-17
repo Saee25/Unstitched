0a3c925048abbc24ec64e108a5f4ff9fda144821
Saee25 <saeenalawade2545@gmail.com>
2025-11-17 22:31:27 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path" // <-- ADD THIS LINE
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Unstitched",
   resolve: { // <-- ADD THIS ENTIRE 'resolve' BLOCK
     alias: {
       "@": path.resolve(__dirname, "./src"),
